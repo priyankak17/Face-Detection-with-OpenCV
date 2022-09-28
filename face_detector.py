d47853860e98582dd2beb383ea4c02d236cb6d25
priyankak17 <31099806+priyankak17@users.noreply.github.com>
2022-09-28 12:54:43 +0530

    Update face_detector.py

--- face_detector.py
+++ face_detector.py
@@ -1,13 +1,13 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  #loading haarcascade xml
-img = cv2.imread('g.jpg')
+img = cv2.imread('news.jpg')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #converting image to grayscale image
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05, minNeighbors = 5) #detect multiscale in gray_image, 
-#scale factor is imagesize downscaled by 50%
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05, minNeighbors = 5) 
+
 
 for x,y,w,h in faces:
-    img = cv2.rectangle(img, (x,y),(x+w,y+h), (0, 255, 0), 3) #detect the face and draw a rectangle around it
+    img = cv2.rectangle(img, (x,y),(x+w,y+h), (0, 255, 0), 3) 
 
 resized = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 
